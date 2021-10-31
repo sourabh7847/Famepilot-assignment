f9bdcc73546e90145b9866345f17da2dec7066e6
sourabh7847 <flank.valish@gmail.com>
2021-10-31 19:13:24 +0530

    logo bug fixed

--- src/components/shared/Sidebar/Sidebar.js
+++ src/components/shared/Sidebar/Sidebar.js
@@ -1,34 +1,39 @@
-import React, { useState } from 'react';
-import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
-import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import SecurityIcon from '@mui/icons-material/Security';
-import { Wrapper, Header, Tab } from './styles';
+import React, { useState } from "react";
+import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import SecurityIcon from "@mui/icons-material/Security";
+import { Wrapper, Header, Tab } from "./styles";
 
 const Sidebar = () => {
-  const [active, setActive] = useState('AccessControl');
+  const [active, setActive] = useState("AccessControl");
   return (
     <Wrapper>
       <Header>
-        <img src="/assets/images/logo.png" alt="" />
+        <img
+          src="https://i.ibb.co/JsQ1Gp7/logo.png"
+          alt=""
+        />
         <h2>MYSITE</h2>
       </Header>
       <Tab
-        className={`${active === 'Projects' && 'active'}`}
-        onClick={() => setActive('Projects')}
+        className={`${active === "Projects" && "active"}`}
+        onClick={() => setActive("Projects")}
       >
         <DashboardOutlinedIcon />
         <p>Projects</p>
       </Tab>
       <Tab
-        className={`${active === 'Org' && 'active'}`}
-        onClick={() => setActive('Org')}
+        className={`${active === "Org" && "active"}`}
+        onClick={() => setActive("Org")}
       >
         <BusinessCenterIcon />
         <p>Organisation Profile</p>
       </Tab>
       <Tab
-        className={`${active === 'AccessControl' && 'active'}`}
-        onClick={() => setActive('AccessControl')}
+        className={`${
+          active === "AccessControl" && "active"
+        }`}
+        onClick={() => setActive("AccessControl")}
       >
         <SecurityIcon />
         <p>Access Control</p>
